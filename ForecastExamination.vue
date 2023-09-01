1e4143cc318e5cf0ef9731bd02de01065dbfc0bb
RayyyyyRGB <134457384+RayyyyyRGB@users.noreply.github.com>
2023-09-01 21:44:56 +0800

    Add files via upload

--- ForecastExamination.vue
+++ ForecastExamination.vue
@@ -1,20 +1,18 @@
 <script setup>
 
-import {onMounted} from "vue";
+import axios from "axios";
 import {ref} from "vue";
-import * as echarts from 'echarts';
-import {nextTick} from "vue";
-import { configProviderContextKey } from "element-plus";
-import VueECharts from 'vue-echarts'
+import {ArrowLeft, ArrowRight} from "@element-plus/icons-vue";
+import VChart from "vue-echarts";
 
 const currentDate = new Date();
 const year = currentDate.getFullYear() - 1 + '';
 const month = currentDate.getMonth() < 10 ? '0' + (currentDate.getMonth() + 1 + '') : currentDate.getMonth() + 1 + ''
 
 
-const SICChartErroPrediction = 'SIC日预测误差'
-const SICChartErroAdd = 'SIC误差统计'
-const SIEChartErroAnalyse = 'SIE误差分析'
+const text_of_option1 = ref('预测误差主要来自于对中纬度和冰岛附近低压的高估，能够预测出NAO的典型两级模态 ，模拟误差随着预测时长逐渐增加。')//表示前六个图底下的文字描述
+
+const text_of_option7 = ref('对于为期1个月的NAOI预测，不如高分辨率模式ECMWF ，但与低分辨率模式ECCC相当。由于只接受月平均数作为输入，忽略了决定短时尺度可预测性的天气现象和初始条件。在超过两个月的提前期的预测技巧远远超过了失去预测能力的数值模式，将NAO的有效预测时间从1个月扩展到了6个月。')
 
 const selectedYear = ref('');
 const selectedMonth = ref('');
@@ -22,210 +20,78 @@ const selectedMonth = ref('');
 selectedYear.value = year;
 selectedMonth.value = month;
 
-let myChart;
-let myChart2;
-let myChart3;
 
 
-const chartTitle = ref('')
-chartTitle.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 预测结果误差折线图`
+const option7 = ref({})
 
-const chartTitle2 = ref('')
-chartTitle2.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 预测结果误差折线图`
+const imgSrc_of_option1 = ref({})
+const imgSrc_of_option2 = ref({})
+const imgSrc_of_option3 = ref({})
+const imgSrc_of_option4 = ref({})
+const imgSrc_of_option5 = ref({})
+const imgSrc_of_option6 = ref({})
 
-const chartTitle3 = ref('')
-chartTitle3.value = `${selectedYear.value}年~${Number(selectedYear.value) + 1 + ''}年 SIC汇报结果误差箱型图`
+const title_of_option1 = ref({})
+const title_of_option2 = ref({})
+const title_of_option3 = ref({})
+const title_of_option4 = ref({})
+const title_of_option5 = ref({})
+const title_of_option6 = ref({})
 
-function updateChartTitle() {
-  chartTitle.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 预测结果误差折线图`;
-  chartTitle2.value = `${selectedYear.value}年${selectedMonth.value}月~${Number(selectedYear.value) + 1 + ''}年${selectedMonth.value}月 预测结果误差折线图`;
-  chartTitle3.value = `${selectedYear.value}年~${Number(selectedYear.value) + 1 + ''}年 SIC汇报结果误差箱型图`;
+//const list = ref({})
 
-  myChart.setOption({
-    title: {
-      text: chartTitle.value,
-      left: 'center' //标题水平居中
-    },
+//axios.get('http://www.tjensoprediction.com:8080/nao/findGridData/nao?year='+Number(selectedYear.value)+'&month='+Number(selectedMonth.value))
+axios.get('http://www.tjensoprediction.com:8080/nao/findGridData/nao?year=2021&month=6')
+    .then(res => {
+      console.log(res.data);
+      list = res.data;
   });
-
-  myChart2.setOption({
-    title: {
-      text: chartTitle2.value,
-      left: 'center' //标题水平居中
-    },
+axios.get('http://www.tjensoprediction.com:8080/nao/predictionExamination/naoi')
+    .then(res => {
+      console.log(res.data);
+      // title_of_option1.value='提前1个月预测';
+      // list = res.data.imgSrc;
+      option7.value = res.data;
   });
-
-  myChart3.setOption({
-    title: {
-      text: chartTitle3.value,
-      left: 'center' //标题水平居中
-    },
-  });
-}
-
-
-
-
-
-
-onMounted(
-    ()=>{
-      nextTick(() => {
-        myChart = echarts.init(document.getElementById('SICChart'));
-        myChart.setOption({
-          title: {
-            text: chartTitle.value,
-            left: 'center' //标题水平居中
-          },
-          tooltip: {},
-          xAxis: {
-            type: 'category',
-            name: '时间',
-            data: ['1月', '2月', '3月', '4月', '5月', '6月']
-          },
-          yAxis: {
-            type: 'value',
-            name: 'RMSE(%)',
-            data: [10, 12, 14, 16, 18]
-          },
-          legend: { //图例
-            data: ['ours', 'persistence'],
-            orient: 'horizontal',
-            left: 'center',
-            bottom: '5',
-          },
-          series: [
-            {
-              name: 'ours',
-              type: 'line',
-              data: [10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-            },
-            {
-              name: 'persistence',
-              type: 'line',
-              data: [2, 2, 12, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-            },
-            
-          ]
-        });
-
-        myChart2 = echarts.init(document.getElementById('SICChart2'));
-        myChart2.setOption({
-          title: {
-            text: chartTitle2.value,
-            left: 'center' //标题水平居中
-          },
-          tooltip: {},
-          xAxis: {
-            type: 'category',
-            name: '时间',
-            data: ['1月', '2月', '3月', '4月', '5月', '6月']
-          },
-          yAxis: {
-            type: 'value',
-            name: 'RMSE(%)',
-            data: [10, 12, 14, 16, 18]
-          },
-          legend: { //图例
-            data: ['ours', 'persistence'],
-            orient: 'horizontal',
-            left: 'center',
-            bottom: '5',
-          },
-          series: [
-            {
-              name: 'ours',
-              type: 'line',
-              data: [10, 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]
-            },
-            {
-              name: 'persistence',
-              type: 'line',
-              data: [2, 2, 12, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-            },
-            
-          ]
-        });
-
-        
-        myChart3 = echarts.init(document.getElementById('SICChart3'));
-        myChart3.setOption({
-          title: {
-            text: chartTitle3.value,
-            left: 'center' //标题水平居中
-          },
-          tooltip: {},
-          xAxis: {
-            type: 'category',
-            name: '时间',
-            data: ['1月', '2月', '3月', '4月', '5月', '6月']
-          },
-          yAxis: {
-            type: 'value',
-            name: 'RMSE(%)',
-            data: [10, 12, 14, 16, 18]
-          },
-          legend: { //图例
-            data: ['ours', 'persistence'],
-            orient: 'horizontal',
-            left: 'center',
-            bottom: '5',
-          },
-          series: [
-            {
-              name: 'ours',
-              type: 'line',
-              data: [10, 10, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]
-            },
-            {
-              name: 'persistence',
-              type: 'line',
-              data: [2, 2, 12, 2, 2, 2, 12, 2, 2, 2, 2, 2]
-            },
-            
-          ]
-          
-        });
-      })
-    }
-)
-
-
-
-
 </script>
 
 <template>
   <div class="pageContent">
     <h1 class="title">
-      {{ selectedYear }}年{{ selectedMonth }}月~{{ Number(selectedYear) + 1 + '' }}年{{ selectedMonth }}月 海冰预测结果检验
+      NAO预测结果检验
     </h1>
     <div class="datePickerContainer">
       <el-date-picker @change="updateChartTitle" v-model="selectedYear" type="year" format="YYYY" value-format="YYYY" :clearable="false" style="width: 80px; height: 25px"/>
       <div class="text">年</div>
       <el-date-picker @change="updateChartTitle" v-model="selectedMonth" type="month" format="MM" value-format="MM" :clearable="false" style="width: 60px; height: 25px"/>
       <div class="text">月</div>
+      <span class="demonstration">提示：选择范围：2015年—2021年</span>
     </div>    
     <el-tabs type="border-card">
-      <el-tab-pane label="SIC日预测误差">
-        <div class="chart" id="SICChart"></div>
-        <div class="chart2" id="SICChart2"></div>
-                
+      <el-tab-pane label="指数预测">
+        <h2 class="title">
+          {{ selectedYear }}年{{ selectedMonth }}月 预测结果分布误差图
+        </h2>
+        <div v-for="i in list">
+          <img :src="i">
+          <!-- <img class="picture" :src="i" alt="">           -->
+        </div>
         <div class="description">
-          {{ SICChartErroPrediction }}
+          {{ text_of_option1 }}
         </div>
       </el-tab-pane>
-      <el-tab-pane label="SIC误差统计">
-        <div class="chart3" id="SICChart3"></div>
+      <el-tab-pane label="模态预测">
+        <h2 class="title">
+           NAOI指数预测的相关系数
+        </h2>
+        <div class="chart">
+          <v-chart :option="option7" autoresize></v-chart>
+        </div>   
         <div class="description">
-          {{ SICChartErroAdd }}
+          {{ text_of_option7 }}
         </div>
       </el-tab-pane>
-      <el-tab-pane label="SIE误差分析">        
-        <div class="description">
-          {{ SIEChartErroAnalyse }}
-        </div>
-      </el-tab-pane>
+     
     </el-tabs>
   </div>
 </template>
@@ -238,17 +104,8 @@ onMounted(
     height: 500px;
   }
 
-  .chart2 {
-    height: 500px;
-  }
-
-  .chart3 {
-    height: 500px;
-    width: 1200px;
-  }
-
   .description {
-    font-size: 16px,
+    font-size: 16px;
   }
 
   .datePickerContainer {
